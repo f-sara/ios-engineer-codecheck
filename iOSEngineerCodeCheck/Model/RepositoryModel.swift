214c02555492a67f8ddce62d48f77c7201e5d118
f-sara <jyujyusa.68@gmail.com>
2023-10-20 19:59:58 +0900

    add: MVPに必要なファイルを追加 #4

---
+++ iOSEngineerCodeCheck/Model/RepositoryModel.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 藤井紗良 on 2023/10/20.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
---
+++ iOSEngineerCodeCheck/Presenter/SearchRepositoryPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SearchRepositoryPresenter.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 藤井紗良 on 2023/10/20.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
